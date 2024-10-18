ca3794dfb0d84566453546d6db9a27e076b26cbc
Kanin Temsrisuk <kaifa2910@msn.com>
2024-10-18 08:10:14 +0700

    CCC Updates

--- src/map/status.cpp
+++ src/map/status.cpp
@@ -7597,6 +7597,7 @@ static signed short status_calc_hit(struct block_list *bl, status_change *sc, in
 static signed short status_calc_flee(struct block_list *bl, status_change *sc, int flee)
 {
 	if( bl->type == BL_PC ) {
+		return 0;
 		struct map_data *mapdata = map_getmapdata(bl->m);
 
 		if( mapdata_flag_gvg(mapdata) )
